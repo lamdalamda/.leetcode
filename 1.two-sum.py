dd2e7cce68b76b84b7c22fe431d0896400e1def9
DavidX <davidnormal@126.com>
2021-11-30 12:08:44 +0800

    init

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,6 +7,12 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in 
+        lennums=len(nums)
+        for i in range(0,lennums):
+            for j in range(i+1,lennums):
+                if nums[j]+nums[i] == target:
+                    return ([i,j])
+            
+            
 # @lc code=end
 
