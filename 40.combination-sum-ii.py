22ca9a035bac4c410502d2a2dcd9376db235b3ee
DavidX <davidnormal@126.com>
2021-12-26 16:18:03 +0800

    90.78%

--- 40.combination-sum-ii.py
+++ 40.combination-sum-ii.py
@@ -12,21 +12,16 @@ class Solution:
     def __init__(self):
         self.hashdict={}
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-
+        
         return self.inner(sorted(candidates),target)
-        pass
+
     def inner(self, candidates, target: int):
         index=0
         combinations=[]
         while index<len(candidates):
-            
-            if index==len(candidates)-1:
-                if candidates[index]==target:
-                    combinations.append([candidates[index]])
-                break
-            if target > candidates[index]:
-                tmp=self.inner(candidates[index+1:],target-candidates[index])
-                for j in tmp:
+
+            if candidates[index]<target:
+                for j in self.inner(candidates[index+1:],target-candidates[index]):
                     combinations.append(j+[candidates[index]])
                 
             if candidates[index]==target:
@@ -38,6 +33,7 @@ class Solution:
             index+=1
             while index<len(candidates) and candidates[index]==candidates[index-1]:
                 index+=1
+                
         return combinations   
         
         
